f9977a7cfd21706b85276a63815e91a8dfadc653
DärcKøder <21283039+nshid@users.noreply.github.com>
2017-08-05 15:19:26 -0500

    Create footer.js

---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <a href="https://www.youtube.com/watch?v=nusgoj74a3Y">React JS Tutorial – Build a website using React, Sass, Gulp and Node.js</a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
